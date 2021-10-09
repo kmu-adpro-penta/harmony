451b536609c526f37825c6a66527496dfaf40b59
jason11489 <75050680+jason11489@users.noreply.github.com>
2021-10-09 14:59:29 +0900

    Create ADD.h

---
+++ code/ADD.h
@@ -0,0 +1,10 @@
+#ifndef __ADD_H__
+#define __ADD_H__
+
+#include "bigint.h"
+
+word* ADD_ABc(word* A, word* B, word c);
+bigint* ADDC(bigint* A, bigint* B);
+bigint* bigint_ADD(bigint* A, bigint* B);
+
+#endif
