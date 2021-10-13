02e01d6f7afe6b551510f6c942e7004977c4ffc1
jason11489 <75050680+jason11489@users.noreply.github.com>
2021-10-13 11:54:03 +0900

    Update ADD.h

--- code/ADD.h
+++ code/ADD.h
@@ -3,7 +3,7 @@
 
 #include "bigint.h"
 
-word* ADD_ABc(word* A, word* B, word c);
+word* ADD_ABc(word A, word B, word c);
 bigint* ADDC(bigint* A, bigint* B);
 bigint* bigint_ADD(bigint* A, bigint* B);
 
