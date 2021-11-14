fd6ffb0aac9faced6cc0ef7d4266384fff4266a4
jason11489 <75050680+jason11489@users.noreply.github.com>
2021-11-14 17:23:13 +0900

    Update DIV.c

--- code/DIV.c
+++ code/DIV.c
@@ -1,1 +1,41 @@
+#include "bigint.h"
+#include <stdio.h>
 
+/*
+
+Multi-Precision Long Division
+
+이진 긴 나눗셈 알고리듬은 다중 워드 단위 긴 나눗셈 DIV로 확장할 수 있다.
+
+2-word long division => DIVCC(A,B) => DIVC(A,B) => DIV(A,B)
+
+
+*/
+
+
+/*
+
+Input : A,B ( 0 < A_j <= W )
+
+Output : Q  ( A = BQ + R ( 0 <= R < B, 0 < Q_j <= W ))
+
+*/
+
+void DIV(bigint* A, bigint* B, bigint** Q, bigint** R) {
+
+	if (A->wordlen < B->wordlen)
+		bi_assign(R, A);
+	else {
+
+		bi_new(R, B->wordlen);
+
+		for (int i = A->wordlen; i > 0; i--) {
+			bi_rshift(R, 1);
+			*((*R)->a) += *(A->a + i);
+			DIVC(R, B, Q);
+
+		}
+	}
+
+
+}
