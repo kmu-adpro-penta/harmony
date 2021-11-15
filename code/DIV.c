0661b01c46e11ae95917cc67ec443d6aa243550b
jason11489 <donny11489@gmail.com>
2021-11-15 11:09:03 +0900

    Update DIV.c

--- code/DIV.c
+++ code/DIV.c
@@ -15,6 +15,23 @@ Multi-Precision Long Division
 
 /*
 
+Input : A, B	( A_j,B_j in [0,W), 0 <= A < B*W
+
+Output : Q, R	( such that A = B*Q + R ( 0 <= R < B , Q in [0,W) )
+
+
+*/
+void DIVC(bigint* A, bigint* B, word* Q_j) {
+
+	if (*(A->a + A->wordlen) < *(B->a + B->wordlen))
+		*Q_j = 0;
+	else {
+
+	}
+}
+
+/*
+
 Input : A,B ( 0 < A_j <= W )
 
 Output : Q  ( A = BQ + R ( 0 <= R < B, 0 < Q_j <= W ))
@@ -29,10 +46,12 @@ void DIV(bigint* A, bigint* B, bigint** Q, bigint** R) {
 
 		bi_new(R, B->wordlen);
 
-		for (int i = A->wordlen; i > 0; i--) {
-			bi_rshift(R, 1);
-			*((*R)->a) += *(A->a + i);
-			DIVC(R, B, Q);
+		for (int i = A->wordlen-1; i > 0; i--) {
+
+			bi_lshift(R, sizeof(word));		// R = R * W
+			*((*R)->a) += *(A->a + i);		// R = R + A_i
+
+			DIVC(R, B, (*Q)->a + i);					// ( Q_i, R ) <- DIVC(R,B)
 
 		}
 	}
